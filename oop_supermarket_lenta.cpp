9200423bc4d142d0eb4752ad1dbbceaef9e033bd
SumanovaViktoriia <Visumanova@gmail.com>
2026-03-01 16:37:06 +0300

    Финальная доработка

--- oop_supermarket_lenta.cpp
+++ oop_supermarket_lenta.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <memory>
+#include <vector>
+
 using namespace std;
 
 class Products {
@@ -10,11 +13,11 @@ public:
         cout << "Products created" << endl;
         this->edible = true;
     }
+
     virtual ~Products() {
         cout << "Products destroyed" << endl;
     }
 
-    // Virtual method for shelf life
     virtual string getShelfLife() {
         return "shelf life not specified";
     }
@@ -24,6 +27,10 @@ class Nature : public Products {
 public:
     Nature() {
         cout << "Nature created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "nature products: shelf life varies";
     }
 };
 
@@ -114,3 +121,117 @@ public:
         return "3-7 days at room temperature";
     }
 };
+
+class Processed : public Products {
+public:
+    Processed() {
+        cout << "Processed created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "processed products: shelf life varies by type";
+    }
+};
+
+class Bakery : public Processed {
+public:
+    Bakery() {
+        cout << "Bakery created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "3-5 days at room temperature";
+    }
+};
+
+class SweetBakery : public Bakery {
+public:
+    SweetBakery() {
+        cout << "SweetBakery created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "2-4 days at room temperature";
+    }
+};
+
+class Bread : public Bakery {
+public:
+    Bread() {
+        cout << "Bread created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "3-5 days at room temperature";
+    }
+};
+
+class Dough : public Bakery {
+public:
+    Dough() {
+        cout << "Dough created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "1-2 days in refrigerator";
+    }
+};
+
+class Offal : public Processed {
+public:
+    Offal() {
+        cout << "Offal created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "1-2 days in refrigerator";
+    }
+};
+
+class Pate : public Offal {
+public:
+    Pate() {
+        cout << "Pate created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "3-5 days in refrigerator after opening";
+    }
+};
+
+class Smoked : public Offal {
+public:
+    Smoked() {
+        cout << "Smoked created" << endl;
+    }
+
+    string getShelfLife() override {
+        return "7-10 days in refrigerator";
+    }
+};
+
+int main() {
+    cout << "CREATING PRODUCTS" << endl;
+
+    vector<unique_ptr<Products>> supermarket;
+
+    supermarket.push_back(make_unique<SweetBakery>());
+    cout << endl;
+
+    supermarket.push_back(make_unique<Bread>());
+    cout << endl;
+
+    supermarket.push_back(make_unique<DairyProducts>());
+    cout << endl;
+
+    supermarket.push_back(make_unique<Cereals>());
+    cout << endl;
+
+    cout << "\nSHELF LIFE INFORMATION" << endl;
+    for (size_t i = 0; i < supermarket.size(); i++) {
+        cout << "Product " << i + 1 << ": " << supermarket[i]->getShelfLife() << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
